3b665b8a73362413a3030dca5d8529e06b43c714
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2020-09-07 20:34:38 +0100

    working as intended

--- src/app.js
+++ src/app.js
@@ -4,24 +4,50 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const imgur = require('imgur-node');
 
-const pictures = 10;
+const words = [
+    ":swan: Honk Honk, Motherfucker.",
+    ":swan: Honk!",
+    ":swan: Honk Hyonk!",
+    ":swan: *HYOOOONK!*",
+    ":swan: HONK HONK! *HYONKKKK!*"
+]
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
 });
 
 client.on('message', msg => {
 
-  if(msg.author.bot){
-      return;
-  }
-  
-  message = msg.content.toLowerCase();
+    if(msg.author.bot){
+        return;
+    }
 
-  if (message.includes("honk") || message.includes("hyonk")) {
-    msg.reply("HYONK");
-  }
+    imgur.album.get(process.env.IMGUR_KEY, '9JJiJWe', (err, res) => {
+
+        if (err) console.error(err);
+
+        imageChoice = Math.floor(Math.random() * res.images.length)
+        wordChoice = Math.floor(Math.random() * words.length)
+
+        const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(words[wordChoice])
+        .setImage(res.images[imageChoice].link)
+        .setFooter('#PricklesIsShit', "https://discord.js.org");
+
+        return msg.channel.send(`<@${msg.author.id}>`, {embed});
+    });
+
+//   numberChoice = Math.floor(Math.random() * pictures);
+
+//   message = msg.content.toLowerCase();
+
+//   if (message.includes("honk") || message.includes("hyonk")) {
+//     msg.reply("HYONK");
+//   }
 
 });
 
